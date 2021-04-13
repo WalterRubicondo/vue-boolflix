29a245d9beef45cabd39f2d2e47edf9f70e62f46
Walter <walterrubicondo@gmail.com>
2021-04-13 12:21:10 +0200

    Fine Milestone 3. Inizio CSS per Milestone 4

---
+++ main.js
@@ -0,0 +1,44 @@
+Vue.config.devtools = true;
+
+var app = new Vue({
+  el: '#root',
+  data: {
+    films: [],
+    search: '',
+  },
+  methods: {
+    searchFilm: function () {
+      axios.get('https://api.themoviedb.org/3/search/movie?api_key=ac8cba58ab0d498ab45ea883868c94c5&', {
+        params: {
+          query: this.search,
+        },
+      }).then((response) => {
+        this.films = [...this.films, ...response.data.results];
+      });
+      axios.get('https://api.themoviedb.org/3/search/tv?api_key=ac8cba58ab0d498ab45ea883868c94c5&', {
+        params: {
+          query: this.search,
+        },
+      }).then((response) => {
+        this.films = [...this.films, ...response.data.results];
+      });
+      this.films = [];
+      this.search = "";
+    },
+     getTitle: function (obj) {
+       if (obj.title) {
+         return obj.title;
+       } else
+        return obj.name;
+     },
+     getOrignalTitle: function (obj) {
+       if (obj.title) {
+         return obj.original_title;
+       } else
+        return obj.original_name;
+     },
+     rating: function (film) {
+       return Math.round(film.vote_average / 2)
+     }
+  }
+});
